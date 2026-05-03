504d06f5924f3d5a8d998db2c0800ce8ac3260fa
sanraal-75 <260251254+sanraal-75@users.noreply.github.com>
2026-05-03 15:50:01 +0100

    L1 done

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ if api_key == None:
 #parse CLI arguments using builtin
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -26,7 +27,9 @@ if response_object.usage_metadata == None:
     raise Exception("Usage metrics returned None!")
 
 #Print token usage to the screen
-print(f"Prompt tokens: {response_object.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response_object.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response_object.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response_object.usage_metadata.candidates_token_count}")
 
 print(response_object.text)
